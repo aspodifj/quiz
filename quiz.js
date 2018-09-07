2fc10a626e99eb880dfd3bbb0a8897a57f52cdb7
aspodifj <42642860+aspodifj@users.noreply.github.com>
2018-09-07 23:47:31 +0900

    add questions add parenthesis

--- quiz.js
+++ quiz.js
@@ -4,6 +4,8 @@ const answer = document.getElementById("answer");
 const element = document.getElementById("element");
 const sound = document.getElementById("sound");
 const sra = document.getElementById("sra");
+const One = document.getElementById("One");
+
 function ring() {
     document.getElementById("sound").play();
 }
@@ -13,7 +15,7 @@ function ring1() {
 all.textContent = "all question: " + a.length;
 
 
-let random = Math.floor(Math.random() * 　a.length);
+let random = Math.floor(Math.random() * a.length);
 let qes = a[random].question;
 let ans = a[random].answer;
 let chars = qes.split("");
@@ -21,16 +23,29 @@ let number = a.indexOf(ans);
 
 let count = 0;
 let timeoutid = null;
-let isRunning = false;
 let index;
+let isReading = false;
+let isAnswering = false;
+let isClicked = false;
+const EXTRA_COUNT = 2;
+let currentExtra;
+let currentIndex;
 
 function tick() {
-    question.textContent +=chars[index];
-    if (index < chars.length - 1) {
+    if (!isClicked && index <= chars.length - 1) {
+        timeoutid = setTimeout(tick, 130);   
+        question.textContent +=chars[index];
+    } else if (isClicked && currentIndex <= currentExtra){
         timeoutid = setTimeout(tick, 130);
-    } else {
-        count = 2;
+        currentIndex++;
+        One.textContent +=chars[index];
+    } else if(!isClicked) {
+        count = 3;
         element.textContent = "答え"
+        isReading = false;
+        isAnswering = false;
+    } else {
+        count = 3;
     }
     index++;
 }
@@ -38,21 +53,36 @@ function tick() {
 element.addEventListener("click", function () {
     count++;
     if (count === 1) {  
+        isReading = true;
+        isAnswering = true;
         question.textContent = random + 1 + "." ;
+        isClicked = false;
         index = 0;
         tick();
         ring();
     } else if (count === 2) {
-        ring1();
+        currentExtra = Math.floor(Math.random()*(EXTRA_COUNT + 1));
+        console.log(currentExtra);
+        currentIndex = 0;      
+        isClicked = true;
+        sra.textContent = "/"
+    } else if (count === 3) {
+        ring1(); 
         clearTimeout(timeoutid);
         element.textContent = "答え"
-    } else if (count === 3) {
-        sra.textContent = "/"  //ここね
+
+    } else if (count === 4) { 
+        //sra.textContent = "/"
+        if (isReading){
+        kac.textContent = ")"
+        }
         answer.textContent = "A. " + ans;
         element.textContent = "次へ"
-        qanswer.textContent =qes.substring(index);
-    } else if (count === 4) {
+        qanswer.textContent = qes.substring(index - 1);
+    } else if (count === 5) {
+        One.textContent = ""
         sra.textContent = ""
+        kac.textContent = ""
         element.textContent = "問題"
         question.textContent = ""
         qanswer.textContent = ""
@@ -63,11 +93,16 @@ element.addEventListener("click", function () {
         chars = qes.split("");
         count = 0;
     }
+    
 });
+
+
+
 
 // function colorchange() {
 //     document.body.style.backgroundColor="#808080";
     
+
 // };
 
 
